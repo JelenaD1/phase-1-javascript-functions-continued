b803d10e73e0992498a7c3e3d7eb35e99d6815ab
Jelena <jelena.durovic@powerhrg.com>
2021-05-21 22:40:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(task = "go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(whatever = "*"){
+    return function(atribute = "special"){
+        return `You are ${whatever}${atribute}${whatever}!`
+
+    }
+}
